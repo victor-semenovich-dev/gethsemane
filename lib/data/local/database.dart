9759ce44a9690019f7a7d139aa7da7ad97d0a96b
Victor Semenovich <victor.semenovich@silkailines.com>
2023-06-19 22:25:03 +0300

    log DB file path

--- lib/data/local/database.dart
+++ lib/data/local/database.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:gethsemane/data/local/model/event.dart';
+import 'package:logging/logging.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -19,6 +20,7 @@ class AppDatabase extends _$AppDatabase {
 LazyDatabase _openConnection() {
   return LazyDatabase(() async {
     final dbFolder = await getApplicationDocumentsDirectory();
+    Logger.root.log(Level.INFO, 'DB file storage path - $dbFolder');
     final file = File(join(dbFolder.path, 'db.sqlite'));
     return NativeDatabase.createInBackground(file);
   });
