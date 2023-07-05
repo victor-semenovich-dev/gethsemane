91872cd7bb96bcccfce84b41d660d3f3ec052fe0
vsemenovich <victor.semenovich@gmail.com>
2023-07-05 18:43:20 +0300

    load initial data only once

--- lib/domain/usecase/load_initial_data_usecase.dart
+++ lib/domain/usecase/load_initial_data_usecase.dart
@@ -1,6 +1,7 @@
 import 'package:gethsemane/domain/repository/authors_repository.dart';
 import 'package:gethsemane/domain/repository/music_groups_repository.dart';
 import 'package:logging/logging.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoadInitialDataUseCase {
   final AuthorsRepository authorsRepository;
@@ -13,8 +14,12 @@ class LoadInitialDataUseCase {
 
   Future<void> invoke() async {
     try {
-      await authorsRepository.loadAuthors();
-      await musicGroupsRepository.loadMusicGroups();
+      final prefs = await SharedPreferences.getInstance();
+      if (prefs.getBool('isInitialDataLoaded') != true) {
+        await authorsRepository.loadAuthors();
+        await musicGroupsRepository.loadMusicGroups();
+        prefs.setBool('isInitialDataLoaded', true);
+      }
     } catch (e) {
       Logger.root.log(Level.SEVERE, e);
     }
