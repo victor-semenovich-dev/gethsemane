97e3e2776306c33e58376688cee61d44b4eb94a3
vsemenovich <victor.semenovich@gmail.com>
2023-07-05 18:25:49 +0300

    adjusted worship titles

--- lib/domain/extensions/datetime.dart
+++ lib/domain/extensions/datetime.dart
@@ -4,7 +4,7 @@ import 'package:intl/intl.dart';
 
 extension DateTimeExt on DateTime {
   String eventTitle(BuildContext context) {
-    final date = DateFormat('dd MMMM yyyy', 'ru').format(this);
+    final date = DateFormat('d MMMM yyyy', 'ru').format(this);
     final weekday = DateFormat('EE', 'ru').format(this).toUpperCase();
     if (this.weekday == DateTime.sunday) {
       if (hour <= 12) {
