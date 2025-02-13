75d9ef84f77ee701ac204bf1c4a055ab59862812
Victor Semenovich <victor.semenovich@gmail.com>
2025-02-13 10:24:52 +0300

    deleted unneeded resources

--- composeApp/src/commonMain/kotlin/by/geth/gethsemane/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package by.geth.gethsemane
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/by/geth/gethsemane/Platform.kt
+++
@@ -1,7 +0,0 @@
-package by.geth.gethsemane
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
