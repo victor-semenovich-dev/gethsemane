ec8d9e824854f0c5259e29cedea60e87624cdf49
Victor Semenovich <victor.semenovich@gmail.com>
2025-02-13 12:53:29 +0300

    implemented the top app bar and fixed edge-to-edge issues

--- composeApp/src/commonMain/kotlin/by/geth/gethsemane/ui/route/home/HomeRoute.kt
+++ composeApp/src/commonMain/kotlin/by/geth/gethsemane/ui/route/home/HomeRoute.kt
@@ -1,18 +1,43 @@
 package by.geth.gethsemane.ui.route.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeRoute() {
-    Box(Modifier.fillMaxSize()) {
-        Text(
-            modifier = Modifier.align(Alignment.Center),
-            text = "Home route",
-        )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier = Modifier
+                    .background(MaterialTheme.colors.primary)
+                    .statusBarsPadding(),
+                elevation = 0.dp,
+                title = {
+                    Text(
+                        text = "Гефсимания",
+                    )
+                },
+            )
+        },
+    ) {
+        Column(
+            modifier = Modifier.padding(horizontal = 8.dp),
+        ) {
+            Button(
+                onClick = {},
+            ) {
+                Text("Расписание")
+            }
+        }
     }
 }
