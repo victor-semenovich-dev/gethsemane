909f511d39b995d67afe6b4679b5793a433d5e6b
Victor Semenovich <victor.semenovich@gmail.com>
2025-02-14 13:04:27 +0300

    refactoring: moved the App to the top level package

--- composeApp/src/commonMain/kotlin/by/geth/gethsemane/App.kt
+++ composeApp/src/commonMain/kotlin/by/geth/gethsemane/App.kt
@@ -1,9 +1,10 @@
-package by.geth.gethsemane.ui
+package by.geth.gethsemane
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import by.geth.gethsemane.di.repositoriesModule
 import by.geth.gethsemane.di.viewModelsModule
+import by.geth.gethsemane.ui.AppNavHost
 import org.koin.compose.KoinApplication
 
 @Composable
--- composeApp/src/iosMain/kotlin/by/geth/gethsemane/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/by/geth/gethsemane/MainViewController.kt
@@ -1,6 +1,5 @@
 package by.geth.gethsemane
 
 import androidx.compose.ui.window.ComposeUIViewController
-import by.geth.gethsemane.ui.App
 
 fun MainViewController() = ComposeUIViewController { App() }
